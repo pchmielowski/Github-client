8a1b3e65cabe1e560f0d6c4179c0057e89b65f78
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-12-03 16:30:16 +0100

    MainActivity cleanup

--- app/src/main/java/net/chmielowski/github/screen/list/SearchActivity.java
+++ app/src/main/java/net/chmielowski/github/screen/list/SearchActivity.java
@@ -40,7 +40,6 @@ public class SearchActivity extends AppCompatActivity {
         ((CustomApplication) getApplication()).component(this).inject(this);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search);
         binding.setModel(model);
-        setSupportActionBar(binding.toolbar);
         binding.list.setLayoutManager(new LinearLayoutManager(this));
         binding.list.setAdapter(adapter);
     }
@@ -76,25 +75,20 @@ public class SearchActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == R.id.action_settings) {
+            startFavouritesActivity();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-
+    private void startFavouritesActivity() {
+//        startActivity(new Intent(this, FavouritesActivity.class));
+    }
 }
