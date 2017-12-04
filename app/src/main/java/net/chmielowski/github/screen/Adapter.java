8bc3e1e63132b54eab5b5678199427bd53b77846
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2017-12-04 20:16:14 +0100

    Adapter refactored

--- app/src/main/java/net/chmielowski/github/screen/Adapter.java
+++ app/src/main/java/net/chmielowski/github/screen/Adapter.java
@@ -28,8 +28,9 @@ import io.reactivex.subjects.Subject;
 
 @ActivityScope
 public final class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private static final int ORDINARY_ELEMENT = 0;
-    private static final int LAST_ELEMENT = 1;
+    private static final int TYPE_REPO = 0;
+    private static final int TYPE_SPINNER = 1;
+
     private final Context context;
     private final List<RepositoryViewModel> items = new ArrayList<>();
 
@@ -40,20 +41,12 @@ public final class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     @Override
     public int getItemViewType(final int position) {
-        return isLastElement(position) ? LAST_ELEMENT : ORDINARY_ELEMENT;
-    }
-
-    private boolean isLastElement(final int position) {
-        return position == items.size();
+        return position == items.size() ? TYPE_SPINNER : TYPE_REPO;
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
-        if (viewType == LAST_ELEMENT) {
-            return new RecyclerView.ViewHolder(inflate(parent, R.layout.item_last).getRoot()) {
-            };
-        }
-        return new ViewHolder(inflate(parent, R.layout.item_repo));
+        return viewType == TYPE_SPINNER ? new SpinnerViewHolder(parent) : new RepoViewHolder(parent);
     }
 
     private <T extends ViewDataBinding> T inflate(final ViewGroup parent, final int layout) {
@@ -69,10 +62,10 @@ public final class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     @Override
     public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
         // TODO: refactor me
-        if (!(holder instanceof ViewHolder)) {
+        if (!(holder instanceof RepoViewHolder)) {
             return;
         }
-        final Adapter.ViewHolder casted = (ViewHolder) holder;
+        final RepoViewHolder casted = (RepoViewHolder) holder;
         final RepositoryViewModel model = items.get(position);
         casted.bind(model);
         RxView.clicks(holder.itemView)
@@ -103,16 +96,26 @@ public final class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         notifyItemRangeInserted(size, repositories.size());
     }
 
-    final class ViewHolder extends RecyclerView.ViewHolder {
+    final class RepoViewHolder extends RecyclerView.ViewHolder {
         private final ItemRepoBinding binding;
 
-        ViewHolder(final ItemRepoBinding binding) {
+        private RepoViewHolder(final ItemRepoBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
 
+        RepoViewHolder(final ViewGroup parent) {
+            this(inflate(parent, R.layout.item_repo));
+        }
+
         private void bind(final RepositoryViewModel model) {
             binding.setModel(model);
+        }
+    }
+
+    final class SpinnerViewHolder extends RecyclerView.ViewHolder {
+        SpinnerViewHolder(final ViewGroup parent) {
+            super(inflate(parent, R.layout.item_spinner).getRoot());
         }
     }
 }
