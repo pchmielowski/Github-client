5cdd7e7eb4ddeb849f24792ad75a6c0bfa36094d
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2017-12-16 16:30:38 +0100

    Test for going offline before timer completes

--- app/src/test/java/net/chmielowski/github/network/NetworkIndicatorViewModelTest.java
+++ app/src/test/java/net/chmielowski/github/network/NetworkIndicatorViewModelTest.java
@@ -19,8 +19,7 @@ public class NetworkIndicatorViewModelTest {
     public void goOfflineAndOnlineAgain() throws Exception {
         final NetworkState state = Mockito.mock(NetworkState.class);
         final Subject<BasicNetworkState.State> networkState = PublishSubject.create();
-        when(state.observe())
-                .thenReturn(networkState);
+        when(state.observe()).thenReturn(networkState);
 
         final CompletableSubject timer = CompletableSubject.create();
 
@@ -39,5 +38,36 @@ public class NetworkIndicatorViewModelTest {
 
         timer.onComplete();
         assertThat(model.visible.get(), is(false));
+    }
+
+    @Test
+    public void goOfflineAndOnlineAndOfflineBeforeTimerCompletes() throws Exception {
+        final NetworkState state = Mockito.mock(NetworkState.class);
+        final Subject<BasicNetworkState.State> networkState = PublishSubject.create();
+        when(state.observe()).thenReturn(networkState);
+
+        final CompletableSubject timer = CompletableSubject.create();
+
+        final NetworkIndicatorViewModel model = new NetworkIndicatorViewModel(state, timer);
+
+        model.start();
+        assertThat(model.visible.get(), is(false));
+
+        networkState.onNext(OFFLINE);
+        assertThat(model.visible.get(), is(true));
+        assertThat(model.state.get(), is(OFFLINE));
+
+        networkState.onNext(ONLINE);
+        assertThat(model.visible.get(), is(true));
+        assertThat(model.state.get(), is(ONLINE));
+
+        networkState.onNext(OFFLINE);
+        assertThat(model.visible.get(), is(true));
+        assertThat(model.state.get(), is(OFFLINE));
+
+        timer.onComplete();
+
+        assertThat(model.visible.get(), is(true));
+        assertThat(model.state.get(), is(OFFLINE));
     }
 }
