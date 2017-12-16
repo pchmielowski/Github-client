adc2bb864d3320204d00b7beb1ee82f0f03e2401
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2017-12-16 14:46:50 +0100

    fetchFailed test

--- app/src/test/java/net/chmielowski/github/data/GithubRepoServiceTest.java
+++ app/src/test/java/net/chmielowski/github/data/GithubRepoServiceTest.java
@@ -7,16 +7,20 @@ import org.mockito.Mockito;
 import java.util.Collection;
 import java.util.HashMap;
 
-import io.reactivex.Single;
 import io.reactivex.observers.TestObserver;
 import retrofit2.Response;
 
+import static io.reactivex.Single.just;
 import static java.util.Collections.singletonList;
 import static net.chmielowski.github.screen.SearchViewModel.Query.firstPage;
 import static net.chmielowski.github.utils.TestUtils.sampleRepository;
+import static okhttp3.MediaType.parse;
+import static okhttp3.ResponseBody.create;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
+import static retrofit2.Response.error;
 
 public class GithubRepoServiceTest {
     private RestService rest;
@@ -34,7 +38,7 @@ public class GithubRepoServiceTest {
 
         final String query = "query";
         when(rest.searchRepositories(query, 0))
-                .thenReturn(Single.just(Response.success(repositories)));
+                .thenReturn(just(Response.success(repositories)));
 
         final GithubRepoService service = new GithubRepoService(rest, new HashMap<>());
         final TestObserver<Collection<Repositories.Item>> testObserver = service
@@ -43,5 +47,29 @@ public class GithubRepoServiceTest {
 
         testObserver.assertValue(repositories.items);
         assertThat(service.cached(repository.fullName), is(repository));
+    }
+
+    @Test
+    public void fetchFailed() throws Exception {
+        final Repositories repositories = new Repositories();
+        final Repositories.Item repository = sampleRepository();
+        repositories.items = singletonList(repository);
+
+        final String query = "query";
+        when(rest.searchRepositories(query, 0))
+                .thenReturn(just(error(404, create(parse("text/plain"), "fake error"))));
+
+        final GithubRepoService service = new GithubRepoService(rest, new HashMap<>());
+        final TestObserver<Collection<Repositories.Item>> testObserver = service
+                .items(firstPage(query))
+                .test();
+
+        testObserver.assertNoValues();
+        testObserver.assertComplete();
+        try {
+            service.cached(repository.fullName);
+            fail("Exception not thrown");
+        } catch (NullPointerException ignored) {
+        }
     }
 }
