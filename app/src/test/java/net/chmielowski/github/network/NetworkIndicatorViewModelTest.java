06669a4651f3ac1a51944b5803e821c008b63500
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2017-12-16 16:35:18 +0100

    NetworkIndicatorViewModelTest cleanup

--- app/src/test/java/net/chmielowski/github/network/NetworkIndicatorViewModelTest.java
+++ app/src/test/java/net/chmielowski/github/network/NetworkIndicatorViewModelTest.java
@@ -1,5 +1,6 @@
 package net.chmielowski.github.network;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,6 +8,7 @@ import io.reactivex.subjects.CompletableSubject;
 import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.Subject;
 
+import static io.reactivex.Completable.never;
 import static net.chmielowski.github.network.BasicNetworkState.State.OFFLINE;
 import static net.chmielowski.github.network.BasicNetworkState.State.ONLINE;
 import static org.hamcrest.CoreMatchers.is;
@@ -15,15 +17,18 @@ import static org.mockito.Mockito.when;
 
 public class NetworkIndicatorViewModelTest {
 
-    @Test
-    public void goOfflineAndOnlineAgain() throws Exception {
-        final NetworkState state = Mockito.mock(NetworkState.class);
-        final Subject<BasicNetworkState.State> networkState = PublishSubject.create();
+    private final NetworkState state = Mockito.mock(NetworkState.class);
+    private final Subject<BasicNetworkState.State> networkState = PublishSubject.create();
+    private final CompletableSubject timer = CompletableSubject.create();
+
+    @Before
+    public void setUp() throws Exception {
         when(state.observe()).thenReturn(networkState);
+    }
 
-        final CompletableSubject timer = CompletableSubject.create();
-
-        final NetworkIndicatorViewModel model = new NetworkIndicatorViewModel(state, timer);
+    @Test
+    public void goesOffline() throws Exception {
+        final NetworkIndicatorViewModel model = new NetworkIndicatorViewModel(state, never());
 
         model.start();
         assertThat(model.visible.get(), is(false));
@@ -31,35 +36,44 @@ public class NetworkIndicatorViewModelTest {
         networkState.onNext(OFFLINE);
         assertThat(model.visible.get(), is(true));
         assertThat(model.state.get(), is(OFFLINE));
+    }
+
+    @Test
+    public void returnsOnline() throws Exception {
+        final NetworkIndicatorViewModel model = new NetworkIndicatorViewModel(state, never());
+
+        model.start();
+
+        networkState.onNext(OFFLINE);
 
         networkState.onNext(ONLINE);
         assertThat(model.visible.get(), is(true));
         assertThat(model.state.get(), is(ONLINE));
+    }
+
+    @Test
+    public void timeout() throws Exception {
+        final NetworkIndicatorViewModel model = new NetworkIndicatorViewModel(state, timer);
+
+        model.start();
+
+        networkState.onNext(OFFLINE);
+
+        networkState.onNext(ONLINE);
 
         timer.onComplete();
         assertThat(model.visible.get(), is(false));
     }
 
     @Test
-    public void goOfflineAndOnlineAndOfflineBeforeTimerCompletes() throws Exception {
-        final NetworkState state = Mockito.mock(NetworkState.class);
-        final Subject<BasicNetworkState.State> networkState = PublishSubject.create();
-        when(state.observe()).thenReturn(networkState);
-
-        final CompletableSubject timer = CompletableSubject.create();
-
+    public void goOfflineAgainBeforeTimeout() throws Exception {
         final NetworkIndicatorViewModel model = new NetworkIndicatorViewModel(state, timer);
 
         model.start();
-        assertThat(model.visible.get(), is(false));
 
         networkState.onNext(OFFLINE);
-        assertThat(model.visible.get(), is(true));
-        assertThat(model.state.get(), is(OFFLINE));
 
         networkState.onNext(ONLINE);
-        assertThat(model.visible.get(), is(true));
-        assertThat(model.state.get(), is(ONLINE));
 
         networkState.onNext(OFFLINE);
         assertThat(model.visible.get(), is(true));
