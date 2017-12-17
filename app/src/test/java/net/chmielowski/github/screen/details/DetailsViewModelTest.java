33159ffb1fbc4c22bf46a7827bec1b1dba87f707
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2017-12-17 14:15:20 +0100

    Test of add/removed from favourites

--- app/src/test/java/net/chmielowski/github/screen/details/DetailsViewModelTest.java
+++ app/src/test/java/net/chmielowski/github/screen/details/DetailsViewModelTest.java
@@ -6,35 +6,41 @@ import net.chmielowski.github.data.Repositories;
 import net.chmielowski.github.screen.details.DetailsViewModel.Action;
 import net.chmielowski.github.utils.TestUtils;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import io.reactivex.observers.TestObserver;
 
+import static io.reactivex.Single.just;
 import static net.chmielowski.github.screen.details.DetailsViewModel.Action.Type.LIKE;
+import static net.chmielowski.github.screen.details.DetailsViewModel.Action.Type.UNLIKE;
 import static org.mockito.Mockito.when;
 
 public class DetailsViewModelTest {
-    private RepoService service;
-    private Favourites liked;
-
-    @Before
-    public void setUp() throws Exception {
-        service = Mockito.mock(RepoService.class);
-        liked = Mockito.mock(Favourites.class);
-    }
 
     @Test
     public void addToFavourites() throws Exception {
+        toggleLike(true, LIKE);
+    }
+
+    @Test
+    public void removeFromFavourites() throws Exception {
+        toggleLike(false, UNLIKE);
+    }
+
+    private void toggleLike(final boolean becomeLiked, final Action.Type expectedAction) {
+        final RepoService service = Mockito.mock(RepoService.class);
+        final Favourites liked = Mockito.mock(Favourites.class);
+
         final Repositories.Item repo = TestUtils.sampleRepository();
         when(service.cached(repo.fullName)).thenReturn(repo);
+        when(liked.toggle(repo)).thenReturn(just(becomeLiked));
 
         final DetailsViewModel model = new DetailsViewModel(service, liked, repo.fullName);
         final TestObserver<Action> testObserver = model.observeActions().test();
 
         model.toggleLike();
 
-        testObserver.assertValue(new Action(LIKE, repo.name));
+        testObserver.assertValue(new Action(expectedAction, repo.name));
     }
 }
