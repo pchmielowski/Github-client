dd9932adc109d2a3511041172dc47b1ba0323a9e
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2017-12-17 20:39:33 +0100

    Picasso with DataBinding

--- app/src/main/java/net/chmielowski/github/DataBindingAdapters.java
+++ app/src/main/java/net/chmielowski/github/DataBindingAdapters.java
@@ -3,11 +3,23 @@ package net.chmielowski.github;
 import android.databinding.BindingAdapter;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public final class DataBindingAdapters {
     @BindingAdapter("visibility")
     public static void setVisibility(final View view, final boolean value) {
         view.setVisibility(value ? View.VISIBLE : View.GONE);
+    }
+
+    @BindingAdapter("avatarUrl")
+    public static void setAvatarImageUrl(final ImageView view, final String url) {
+        Picasso.with(view.getContext())
+                .load(url)
+                .placeholder(R.drawable.ic_avatar_placeholder)
+                .fit()
+                .into(view);
     }
 
     @BindingAdapter("visibility")
--- app/src/main/java/net/chmielowski/github/screen/details/DetailsActivity.java
+++ app/src/main/java/net/chmielowski/github/screen/details/DetailsActivity.java
@@ -6,8 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 
-import com.squareup.picasso.Picasso;
-
 import net.chmielowski.github.CustomApplication;
 import net.chmielowski.github.R;
 import net.chmielowski.github.databinding.ActivityDetailsBinding;
@@ -37,13 +35,6 @@ public class DetailsActivity extends BaseActivity {
         final ActivityDetailsBinding binding = DataBindingUtil
                 .setContentView(this, R.layout.activity_details);
         binding.setModel(model);
-
-        // TODO: use DataBinding
-        Picasso.with(this)
-                .load(model.avatar.get())
-                .placeholder(R.drawable.ic_avatar_placeholder)
-                .fit()
-                .into(binding.avatar);
     }
 
     @NonNull
