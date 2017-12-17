8c2048e0d464a1feb4181cfff43e103789920781
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2017-12-17 16:54:40 +0100

    Unused method

--- app/src/main/java/net/chmielowski/github/DataBindingAdapters.java
+++ app/src/main/java/net/chmielowski/github/DataBindingAdapters.java
@@ -3,9 +3,6 @@ package net.chmielowski.github;
 import android.databinding.BindingAdapter;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.BounceInterpolator;
-import android.view.animation.RotateAnimation;
 
 public final class DataBindingAdapters {
     @BindingAdapter("visibility")
@@ -21,20 +18,4 @@ public final class DataBindingAdapters {
             view.hide();
         }
     }
-
-    @BindingAdapter("rotating")
-    public static void setFabRotating(final FloatingActionButton view, final boolean value) {
-        if (value) {
-            final Animation animation = new RotateAnimation(0, 360,
-                    Animation.RELATIVE_TO_SELF, 0.5f,
-                    Animation.RELATIVE_TO_SELF, 0.5f);
-            animation.setDuration(2000);
-            animation.setRepeatCount(Animation.INFINITE);
-            animation.setInterpolator(new BounceInterpolator());
-            view.startAnimation(animation);
-        } else {
-            view.clearAnimation();
-        }
-    }
-
 }
