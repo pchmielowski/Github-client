d0e9f881170bf786b1460d97326c2228b718aa2c
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2017-12-18 12:21:09 +0100

    Some providers in NetworkModule

--- app/src/main/java/net/chmielowski/github/ApplicationModule.java
+++ app/src/main/java/net/chmielowski/github/ApplicationModule.java
@@ -1,15 +1,9 @@
 package net.chmielowski.github;
 
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.support.v4.content.LocalBroadcastManager;
-
-import com.google.android.gms.gcm.GcmNetworkManager;
 
 import dagger.Module;
 import dagger.Provides;
-
-import static android.content.Context.CONNECTIVITY_SERVICE;
 
 @Module
 class ApplicationModule {
@@ -25,18 +19,4 @@ class ApplicationModule {
         return context;
     }
 
-    @Provides
-    static ConnectivityManager provideConnectivityManager(@ApplicationContext final Context context) {
-        return (ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);
-    }
-
-    @Provides
-    static GcmNetworkManager provideGcmNetworkManager(@ApplicationContext final Context context) {
-        return GcmNetworkManager.getInstance(context);
-    }
-
-    @Provides
-    static LocalBroadcastManager provideBroadcastManager(@ApplicationContext final Context context) {
-        return LocalBroadcastManager.getInstance(context);
-    }
 }
--- app/src/main/java/net/chmielowski/github/network/NetworkModule.java
+++ app/src/main/java/net/chmielowski/github/network/NetworkModule.java
@@ -1,11 +1,37 @@
 package net.chmielowski.github.network;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.google.android.gms.gcm.GcmNetworkManager;
+
+import net.chmielowski.github.ApplicationContext;
+
 import dagger.Binds;
 import dagger.Module;
+import dagger.Provides;
+
+import static android.content.Context.CONNECTIVITY_SERVICE;
 
 @Module
 public abstract class NetworkModule {
     @Binds
     abstract NetworkState bindNetworkState(BasicNetworkState impl);
+
+    @Provides
+    public static ConnectivityManager provideConnectivityManager(@ApplicationContext final Context context) {
+        return (ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);
+    }
+
+    @Provides
+    public static GcmNetworkManager provideGcmNetworkManager(@ApplicationContext final Context context) {
+        return GcmNetworkManager.getInstance(context);
+    }
+
+    @Provides
+    public static LocalBroadcastManager provideBroadcastManager(@ApplicationContext final Context context) {
+        return LocalBroadcastManager.getInstance(context);
+    }
 
 }
